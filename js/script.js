cea328fac19cfecabbec6a86a8d32d5a12459f13
Muhammad Usman Javed <usman3858156@gmail.com>
2022-08-18 11:36:14 +0500

    2nd commit

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ const qr = document.createElement('qrcode');
 
 const onGenerateSubmit=(e) => {
     e.preventDefault();
-
+    clearUI();
 
 const url = document.getElementById('url').value;
 const size = document.getElementById('size').value;
@@ -17,6 +17,12 @@ setTimeout(() => {
 hideSpinner();
 generateQRCode(url, size);
 },1000)
+
+setTimeout(() =>{
+    const saveURL =qr.querySelector('img').src;
+
+},50)
+createSaveBtn(saveURL);
 
 }
 };
@@ -40,4 +46,27 @@ const showSpinner = (e) =>{
 const hideSpinner = (e) =>{
     spinner.style.display = 'none';
 }
+
+// Clear UI
+
+const clearUI = (e) =>{
+    qr.innerHTML = '';
+    const saveLink = document.getElementById('save-link'); 
+    if (saveLink) saveLink.remove();
+}
+const createSaveBtn = (saveURL) =>{
+
+const link = document.createElement('a');
+link.id = 'save-link';
+link.classList= 'bg-red-500 hover:bg-red-700 text-white font-bold py-2 rounded w-1/3 m-auto my-5';
+link.href = saveURL;
+link.download = 'qrcode';
+link.innerHTML = 'Download QR Code';
+document.getElementById('generated').appendChild(link);
+}
+
+hideSpinner();
+
+
+
 form.addEventListener('submit',onGenerateSubmit);
